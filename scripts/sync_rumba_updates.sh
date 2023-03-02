f4f8617589623902278a150817f334d0abf81d3c
Claas Augner <caugner@mozilla.com>
2023-03-02 20:00:11 +0100

    fix(sync_rumba_updates): use jq -r param
    Outputs raw strings, not JSON texts.

--- scripts/sync_rumba_updates.sh
+++ scripts/sync_rumba_updates.sh
@@ -1,8 +1,8 @@
 updates_host=${UPDATES_HOST?};
 api_host=${RUMBA_HOST?};
 
-most_recent_update=`curl --fail $updates_host/rumba-bcd-updates/bcd-updates.json | jq '[.browsers[].releases[] | select(.status == "current")] | max_by(.release_date) | .release_date'`
-most_recent_api_release=`curl --fail $api_host/api/v2/updates/ | jq '.data[0].release_date'`
+most_recent_update=`curl --fail $updates_host/rumba-bcd-updates/bcd-updates.json | jq -r '[.browsers[].releases[] | select(.status == "current")] | max_by(.release_date) | .release_date'`
+most_recent_api_release=`curl --fail $api_host/api/v2/updates/ | jq -r '.data[0].release_date'`
 if [ $(date -d $most_recent_update +%s) -gt $(date -d $most_recent_api_release +%s) ]; 
     then 
         echo "$most_recent_update is more recent than $most_recent_api_release";
