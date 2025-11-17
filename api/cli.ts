faab005ddbf84f01886dd114b1b71dfc9166bab6
Claas Augner <495429+caugner@users.noreply.github.com>
2025-11-17 12:40:52 +0100

    chore(api): sync only `/current` folder (#221)

--- api/cli.ts
+++ api/cli.ts
@@ -1,6 +1,6 @@
 import { mkdir, writeFile } from "node:fs/promises";
 import { major } from "semver";
-import { bcdAPIs, bcdVersion, walk } from "./index";
+import { bcdAPIs, walk } from "./index";
 import { version } from "./package.json";
 
 const BUILD_PATH = `out/v${major(version)}`;
@@ -9,11 +9,9 @@ main();
 
 async function main() {
   await mkdir(`${BUILD_PATH}/current`, { recursive: true });
-  await mkdir(`${BUILD_PATH}/${bcdVersion}`, { recursive: true });
   walk(bcdAPIs, "", (data) => {
     const json = JSON.stringify(data);
     const filename = `${data.query}.json`;
     writeFile(`${BUILD_PATH}/current/${filename}`, json);
-    writeFile(`${BUILD_PATH}/${bcdVersion}/${filename}`, json);
   });
 }
